a5fb003eafe8220c2483517da6adc996b661caab
Justin Dang <dangjustintime@gmail.com>
2026-01-14 19:01:06 -0500

    initialing go module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dangjustintime/simple-go-lang-sever
+
+go 1.25.5
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world!", r.URL.Path[1:])
+	fmt.Fprintf(w, "hello world!")
 }
 
 func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", helloWorldHandler).Methods("GET")
 	http.HandleFunc("/", helloWorldHandler)
+
 	fmt.Println("running server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Failed to start server:", err)
